0aaf3ffef25be488f3fc0475e05a2654c5c0c8cc
Brad Howes <bradhowes@mac.com>
2020-03-01 17:44:15 +0100

    Removed AppleScript stuff

--- BlockCommentApp/ViewController.swift
+++ BlockCommentApp/ViewController.swift
@@ -11,29 +11,9 @@ class ViewController: NSViewController {
         view.layer?.backgroundColor = NSColor(calibratedWhite: 0.1, alpha: 1.0).cgColor
     }
 
-    override var representedObject: Any? {
-        didSet {
-            // Update the view, if already loaded.
-        }
-    }
-
-    func openExtensionWithScript() {
-        let source = """
-tell application "System Preferences"
-    activate
-    reveal anchor "Extensions" of pane id "com.apple.preferences.extensions"
-end tell
-"""
-        let script = NSAppleScript(source: source)!
-        var error: NSDictionary?
-        script.executeAndReturnError(&error)
-        print("\(error?.description ?? "NA")")
-    }
-
     @IBAction func openExtensions(_ sender: Any) {
-        openExtensionWithScript()
-        // let url = URL(string: "x-apple.systempreferences:com.apple.preferences")!
-        // NSWorkspace.shared.open(url)
+        let url = URL(string: "x-apple.systempreferences:com.apple.preferences")!
+        NSWorkspace.shared.open(url)
     }
 
     @IBAction func quitApp(_ sender: Any) {
